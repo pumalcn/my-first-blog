9ad5dd79263b93c26f1a380e9df85737958f3723
pumalcn <pumalcn03@gmail.com>
2024-01-23 21:22:50 +0600

    Rewrited post_new view

--- blog/views.py
+++ blog/views.py
@@ -15,7 +15,17 @@ def post_detail(request, pk):
 
 
 def post_new(request):
-    form = PostForm()
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_at = timezone.now()
+            post.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+
     return render(request, 'blog/post_edit.html', {'form': form})
 
 
